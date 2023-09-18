74f31559f910fb0f81567c7dcd3d78c01bbe5bc8
Ningaanza Remy <remimaradona@gmail.com>
2023-09-17 21:28:03 -0700

    Add basic HomePage component structure

---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+      <div className="home">
+        Welcome to FinTech Social!
+      </div>
+  );
+}
+
+export default HomePage;
