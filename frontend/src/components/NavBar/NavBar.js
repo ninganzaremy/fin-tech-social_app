586546910a2519ee72404b80b7a37f100a78e80a
Ningaanza Remy <remimaradona@gmail.com>
2023-09-17 21:35:57 -0700

    add global navBar

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const NavBar = () => {
+  return (
+      <div className="navbar">
+        <div className="navbar-logo">FinTech Social</div>
+        <div className="navbar-links">
+          <a href="/">Home</a>
+          <a href="/login">Login</a>
+          <a href="/signup">Signup</a>
+        </div>
+      </div>
+  );
+}
+
+export default NavBar;
